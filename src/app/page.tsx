0de0929193f274b9f44e0a9bf9377d26b29db158
jangiwalia <jangiwalia83@gmail.com>
2024-10-05 16:48:23 -0700

    city links added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
 import { Typography, Box, TextField, Container } from "@mui/material";
+import Link from "next/link";
 
+const popularLocations = [
+  {
+    name: "Surrey",
+    url: "/events/surrey",
+  },
+  {
+    name: "Delta",
+    url: "/events/delta",
+  },
+];
 export default function Home() {
   return (
     <Container maxWidth="sm">
@@ -19,30 +30,44 @@ export default function Home() {
           This is a simple web application{" "}
           <span className="text-accent">to help you sell</span> your items.
         </Typography>
-        <TextField
-          autoFocus={false}
-          color="primary"
-          className="rounded-lg"
-          fullWidth
-          id="outlined-basic"
-          placeholder="Search for items"
-          sx={{
-            "& .MuiOutlinedInput-root": {
-              "&:hover fieldset": {
-                borderColor: "rgba(255, 255, 255, 0.9)", // Slightly brighter on hover
+        <form className="w-full">
+          <TextField
+            autoFocus={false}
+            color="primary"
+            className="rounded-lg"
+            fullWidth
+            id="outlined-basic"
+            placeholder="Search for items"
+            sx={{
+              "& .MuiOutlinedInput-root": {
+                "&:hover fieldset": {
+                  borderColor: "rgba(255, 255, 255, 0.9)", // Slightly brighter on hover
+                },
+                "&.Mui-focused fieldset": {
+                  borderColor: "#a4f839", // Full white when focused
+                },
               },
-              "&.Mui-focused fieldset": {
-                borderColor: "#a4f839", // Full white when focused
+              "& .MuiInputBase-input": {
+                color: "rgba(255, 255, 255, 0.9)", // Off-white color for the text
               },
-            },
-            "& .MuiInputBase-input": {
-              color: "rgba(255, 255, 255, 0.9)", // Off-white color for the text
-            },
-            "& .MuiInputLabel-root": {
-              color: "rgba(255, 255, 255, 0.7)", // Off-white color for the label (if you add one)
-            },
-          }}
-        />
+              "& .MuiInputLabel-root": {
+                color: "rgba(255, 255, 255, 0.7)", // Off-white color for the label (if you add one)
+              },
+            }}
+          />
+        </form>
+        <section className="flex gap-x-4 justify-around w-full">
+          <Typography variant="caption">Popular Location</Typography>
+          <ul className="flex gap-x-4 text-sm text-white/60">
+            {popularLocations.map((location, index) => (
+              <li key={index}>
+                <Link className="hover:text-white" href={location.url}>
+                  {location.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </section>
       </Box>
     </Container>
   );
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 function Logo() {
   return (
-    <Image
-      width={36}
-      height={20}
-      src="/Image.png"
-      alt="Garage Sale"
-      className="mr-2"
-    />
+    <Link href="/">
+      <Image
+        width={36}
+        height={20}
+        src="/Image.png"
+        alt="Garage Sale"
+        className="mr-2"
+      />
+    </Link>
   );
 }
 
