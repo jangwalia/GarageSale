3e7ee7e28a072e5085f38961ebf30938abad6cfd
jangiwalia <jangiwalia83@gmail.com>
2024-10-12 11:55:06 -0700

    ALA_0002_added search form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import { Typography, Box, TextField, Container } from "@mui/material";
+import { Typography, Box, Container } from "@mui/material";
+import SearchForm from "@/components/SearchForm";
 import Link from "next/link";
 
 const popularLocations = [
@@ -30,32 +31,7 @@ export default function Home() {
           This is a simple web application{" "}
           <span className="text-accent">to help you sell</span> your items.
         </Typography>
-        <form className="w-full">
-          <TextField
-            autoFocus={false}
-            color="primary"
-            className="rounded-lg"
-            fullWidth
-            id="outlined-basic"
-            placeholder="Search for items"
-            sx={{
-              "& .MuiOutlinedInput-root": {
-                "&:hover fieldset": {
-                  borderColor: "rgba(255, 255, 255, 0.9)", // Slightly brighter on hover
-                },
-                "&.Mui-focused fieldset": {
-                  borderColor: "#a4f839", // Full white when focused
-                },
-              },
-              "& .MuiInputBase-input": {
-                color: "rgba(255, 255, 255, 0.9)", // Off-white color for the text
-              },
-              "& .MuiInputLabel-root": {
-                color: "rgba(255, 255, 255, 0.7)", // Off-white color for the label (if you add one)
-              },
-            }}
-          />
-        </form>
+        <SearchForm />
         <section className="flex gap-x-4 justify-around w-full">
           <Typography variant="caption">Popular Location</Typography>
           <ul className="flex gap-x-4 text-sm text-white/60">
