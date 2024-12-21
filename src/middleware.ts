ac91af94910657a5885fda6044763103e3d421bb
jangiwalia <jangiwalia83@gmail.com>
2024-12-20 18:39:33 -0800

    ALA-2002-changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,3 +3,9 @@ import { NextResponse } from "next/server";
 export function middleware(req: Request) {
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
+};
